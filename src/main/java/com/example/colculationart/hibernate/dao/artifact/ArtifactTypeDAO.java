bdba09098c801e665b0daf9132cd37e41b844194
Richeansss <pavelcutov@gmail.com>
2024-07-03 20:31:50 +0700

    Create entity and DAO

---
+++ src/main/java/com/example/colculationart/hibernate/dao/artifact/ArtifactTypeDAO.java
@@ -0,0 +1,8 @@
+package com.example.colculationart.hibernate.dao.artifact;
+
+import com.example.colculationart.hibernate.dao.character.GenericDAO;
+import com.example.colculationart.hibernate.entity.ArtifactType;
+
+public class ArtifactTypeDAO extends GenericDAO<ArtifactType> {
+    public ArtifactTypeDAO() {super(ArtifactType.class);}
+}
---
+++ src/main/java/com/example/colculationart/hibernate/entity/ArtifactType.java
@@ -0,0 +1,24 @@
+package com.example.colculationart.hibernate.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class ArtifactType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "artifact_set_id", nullable = false)
+    private ArtifactSet artifactSet;
+
+    private String flower;
+    private String plume;
+    private String sands;
+    private String goblet;
+    private String circlet;
+}
